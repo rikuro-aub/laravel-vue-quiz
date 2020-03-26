abf14d6216cc25ea629a82abd002e0baa87e0efc
3no3 <ulis7524@gmail.com>
2020-03-27 01:46:01 +0900

    browserSyncの設定を追加

--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -2,7 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="/public/css/app.css" rel="stylesheet">
   <title>{{ config('app.name') }}</title>
 
   <!-- Scripts -->
